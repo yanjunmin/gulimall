164ad7fe383c143fe33e0970d672c56484ccb4df
yanjunmin <1519484516@qq.com>
2020-05-18 21:33:18 +0800

    update

---
+++ gulimall-order/src/test/java/top/westyle/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package top.westyle.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-product/src/test/java/top/westyle/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package top.westyle.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
