fd265aa2a2c7fa7434f1c60c142da3c3bcaf3736
yjm <1519484516@qq.com>
2020-05-25 22:40:50 +0800

    update

--- gulimall-coupon/src/test/java/top/westyle/gulimall/coupon/GulimallCouponApplicationTests.java
+++ gulimall-coupon/src/test/java/top/westyle/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -3,11 +3,14 @@ package top.westyle.gulimall.coupon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @SpringBootTest
 class GulimallCouponApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
     }
 
 }
---
+++ gulimall-coupon/src/test/java/top/westyle/gulimall/coupon/MyTest.java
@@ -0,0 +1,9 @@
+package top.westyle.gulimall.coupon;
+
+import java.util.UUID;
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
+    }
+}
